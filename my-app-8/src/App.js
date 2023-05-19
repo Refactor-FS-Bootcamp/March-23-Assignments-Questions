3b1acb773974027e5f941d4025765b584e244df9
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-05-19 16:25:57 +0530

    Add files via upload

---
+++ my-app-8/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import GrandParent from './components/GrandParent';
+import { ChannelProvider } from './components/channelContext';
+import { UserProvider } from './components/userContext';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProvider value="Nabendu">
+        <ChannelProvider value="Refactor">
+          <GrandParent />
+        </ChannelProvider>
+      </UserProvider>
+    </div>
+  );
+}
+
+export default App;
