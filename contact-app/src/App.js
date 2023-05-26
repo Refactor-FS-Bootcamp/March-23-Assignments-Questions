2f939e1b0fa8fccaf1c6a63a4b10512a0a06d181
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-05-26 16:39:24 +0530

    Add files via upload

---
+++ contact-app/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import AddContact from './components/AddContact';
+import ContactList from './components/ContactList';
+import Header from './components/Header';
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = contact => {
+    setContacts([...contacts, contact])
+  }
+
+  console.log(contacts)
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
+
+export default App;
